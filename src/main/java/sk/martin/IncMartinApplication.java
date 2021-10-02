ca0f7ba052f23c8876a077c88939025341d6ea7d
martin <martinbabiak93@gmail.com>
2021-10-02 18:33:10 +0200

    Inicializacia projektu

---
+++ src/main/java/sk/martin/IncMartinApplication.java
@@ -0,0 +1,13 @@
+package sk.martin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncMartinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncMartinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sk/martin/IncMartinApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.martin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncMartinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
